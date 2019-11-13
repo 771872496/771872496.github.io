13ff295ed9863ffeb2be9d1ca84f812aff5851cd
771872496 <771872496@qq.com>
2019-11-13 20:24:07 +0800

    Site updated: 2019-11-13 20:24:06

--- js/post-details.js
+++ js/post-details.js
@@ -1,3 +1,118 @@
-// build time:Wed Nov 13 2019 20:12:21 GMT+0800 (GMT+08:00)
-$(document).ready(function(){function e(){var e=".post-toc";var o=$(e);var a=".active-current";function t(){$(e+" "+a).removeClass(a.substring(1))}o.on("activate.bs.scrollspy",function(){var a=$(e+" .active").last();t();a.addClass("active-current");o.scrollTop(a.offset().top-o.offset().top+o.scrollTop()-o.height()/2)}).on("clear.bs.scrollspy",t);$("body").scrollspy({target:e})}e()});$(document).ready(function(){var e=200;$(".sidebar-nav li").on("click",function(){var o=$(this);var a="sidebar-nav-active";var t="sidebar-panel-active";if(o.hasClass(a)){return}var r=$("."+t);var s=$("."+o.data("target"));r.animate({opacity:0},e,function(){r.hide();s.stop().css({opacity:0,display:"block"}).animate({opacity:1},e,function(){r.removeClass(t);s.addClass(t)})});o.siblings().removeClass(a);o.addClass(a)});$(".post-toc a").on("click",function(e){e.preventDefault();var o=NexT.utils.escapeSelector(this.getAttribute("href"));var a=$(o).offset().top;$("html, body").stop().animate({scrollTop:a},500)});var o=$(".post-toc-content");var a=CONFIG.page.sidebar;if(typeof a!=="boolean"){var t=CONFIG.sidebar.display==="post"||CONFIG.sidebar.display==="always";var r=o.length>0&&o.html().trim().length>0;a=t&&r}if(a){CONFIG.motion.enable?NexT.motion.middleWares.sidebar=function(){NexT.utils.displaySidebar()}:NexT.utils.displaySidebar()}});function createImgEventFullScreen(){var e=$(".post-body").find("img");console.log(e);for(var o=0;o<e.length;o++){e[o].onclick=function(e){var o=e.srcElement.currentSrc;a(o)}}function a(e){console.log(e);var o=$("<div id='fullScreenCover' class='zhao-cover-img-container'><img class='zhao-cover-img' src='"+e+"'/></div>");$("#fullScreenCover").remove();$("body").append(o);$("body").addClass("zhao-no-scroll");$("#fullScreenCover").click(function(){$("#fullScreenCover").remove();$("body").removeClass("zhao-no-scroll")})}}setTimeout(function(){createImgEventFullScreen()},1e3);
-//rebuild by neat 
+/* global NexT, CONFIG */
+
+$(document).ready(function() {
+
+  function initScrollSpy() {
+    var tocSelector = '.post-toc';
+    var $tocElement = $(tocSelector);
+    var activeCurrentSelector = '.active-current';
+
+    function removeCurrentActiveClass() {
+      $(tocSelector + ' ' + activeCurrentSelector)
+        .removeClass(activeCurrentSelector.substring(1));
+    }
+
+    $tocElement
+      .on('activate.bs.scrollspy', function() {
+        var $currentActiveElement = $(tocSelector + ' .active').last();
+
+        removeCurrentActiveClass();
+        $currentActiveElement.addClass('active-current');
+
+        // Scrolling to center active TOC element if TOC content is taller then viewport.
+        $tocElement.scrollTop($currentActiveElement.offset().top - $tocElement.offset().top + $tocElement.scrollTop() - ($tocElement.height() / 2));
+      })
+      .on('clear.bs.scrollspy', removeCurrentActiveClass);
+
+    $('body').scrollspy({ target: tocSelector });
+  }
+
+  initScrollSpy();
+});
+
+$(document).ready(function() {
+  var TAB_ANIMATE_DURATION = 200;
+
+  $('.sidebar-nav li').on('click', function() {
+    var item = $(this);
+    var activeTabClassName = 'sidebar-nav-active';
+    var activePanelClassName = 'sidebar-panel-active';
+    if (item.hasClass(activeTabClassName)) {
+      return;
+    }
+
+    var currentTarget = $('.' + activePanelClassName);
+    var target = $('.' + item.data('target'));
+
+    currentTarget.animate({ opacity: 0 }, TAB_ANIMATE_DURATION, function() {
+      currentTarget.hide();
+      target
+        .stop()
+        .css({'opacity': 0, 'display': 'block'})
+        .animate({ opacity: 1 }, TAB_ANIMATE_DURATION, function() {
+          currentTarget.removeClass(activePanelClassName);
+          target.addClass(activePanelClassName);
+        });
+    });
+
+    item.siblings().removeClass(activeTabClassName);
+    item.addClass(activeTabClassName);
+  });
+
+  // TOC item animation navigate & prevent #item selector in adress bar.
+  $('.post-toc a').on('click', function(e) {
+    e.preventDefault();
+    var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
+    var offset = $(targetSelector).offset().top;
+
+    $('html, body').stop().animate({
+      scrollTop: offset
+    }, 500);
+  });
+
+  // Expand sidebar on post detail page by default, when post has a toc.
+  var $tocContent = $('.post-toc-content');
+  var display = CONFIG.page.sidebar;
+  if (typeof display !== 'boolean') {
+    // There's no definition sidebar in the page front-matter
+    var isSidebarCouldDisplay = CONFIG.sidebar.display === 'post'
+     || CONFIG.sidebar.display === 'always';
+    var hasTOC = $tocContent.length > 0 && $tocContent.html().trim().length > 0;
+    display = isSidebarCouldDisplay && hasTOC;
+  }
+  if (display) {
+    CONFIG.motion.enable
+      ? NexT.motion.middleWares.sidebar = function() {
+        NexT.utils.displaySidebar();
+      }
+      : NexT.utils.displaySidebar();
+  }
+});
+
+//----自定义js----------------
+function createImgEventFullScreen() {
+  var imgs = $(".post-body").find("img");
+  console.log(imgs);
+  for(var i = 0;i < imgs.length;i++) {
+    // $(imgs[i]).click(createCover(imgs[i]));
+    imgs[i].onclick= function(e) {
+      var src = e.srcElement.currentSrc;
+      createCover(src)
+    }
+  }
+
+  function createCover (src) {
+    console.log(src);
+    var cover = $("<div id='fullScreenCover' class='zhao-cover-img-container'><img class='zhao-cover-img' src='"+src+"'/></div>");
+    $("#fullScreenCover").remove();
+    $("body").append(cover);
+    $("body").addClass("zhao-no-scroll");
+    $("#fullScreenCover").click(function(){
+      $("#fullScreenCover").remove();
+      $("body").removeClass("zhao-no-scroll");
+    })
+  }
+}
+setTimeout(function(){
+  createImgEventFullScreen();
+},1000)
