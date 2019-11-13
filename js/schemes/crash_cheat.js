2f39b1e3182ea4af7487ee4295b0d0327becc9b8
771872496 <771872496@qq.com>
2019-11-13 20:12:28 +0800

    Site updated: 2019-11-13 20:12:27

--- js/schemes/crash_cheat.js
+++ js/schemes/crash_cheat.js
@@ -1,16 +1,3 @@
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+// build time:Wed Nov 13 2019 20:12:21 GMT+0800 (GMT+08:00)
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+//rebuild by neat 
